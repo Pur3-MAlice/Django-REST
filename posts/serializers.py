47275003cd46a0def0a2273acef83d12097920c0
Pur3-MAlice <aliceford98@live.co.uk>
2023-11-16 10:03:05 +0000

    adds in post update and delete function

--- posts/serializers.py
+++ posts/serializers.py
@@ -30,7 +30,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'is_owner', 'profile_id',
-            'profile_image'
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter'
         ]
